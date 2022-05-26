84a7600d7898623abedfbbebc3e3e417b4e29cbd
Alexandru Prodan <prodanalexandru580@gmail.com>
2022-05-26 19:56:52 +0300

    basic slice setup and useSelector example

---
+++ demo-app/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { CartIcon } from "../Icons";
+import { useSelector } from "react-redux";
+
+export default function Navbar() {
+  const { amount } = useSelector(state => state.cart);
+  return (
+    <nav>
+        <div className="nav-center">
+            <h3>Redux toolkit</h3>
+            <div className="nav-container">
+                <CartIcon />
+                <div className="amount-container">
+                    <p className="total-amount"> { amount }</p>
+                </div>
+            </div>
+            
+        </div>
+    </nav>
+  )
+}
